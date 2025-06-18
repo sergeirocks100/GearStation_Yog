d9e104c6362ae0431bbfa58ec50b12bb99653c61
sergeirocks100 <31294280+sergeirocks100@users.noreply.github.com>
2025-06-18 17:24:24 -0400

    balls, meet tack hammer
    change auxmos from being compiled by the CI, to being downloaded from the repository
    
    the compile and run tests keep hanging, and i can't figure out why
    
    i know it's not rust-g, because i copied the .dll from the fork i made into my local folder, and the game ran without any apparent issues

--- tools/travis/install_extools.sh
+++ tools/travis/install_extools.sh
@@ -9,14 +9,9 @@ then
   echo "Using cached directory."
 else
   echo "Installing Auxmos"
-  git clone https://github.com/Putnam3145/auxmos
-  cd ./auxmos
-  rustup target add i686-unknown-linux-gnu
-  cargo build --target i686-unknown-linux-gnu --features katmos --release
-  chmod 755 ./target/i686-unknown-linux-gnu/release/libauxmos.so
-  
+  curl "https://github.com/Putnam3145/auxmos/releases/download/${AUXMOS_VERSION}/libauxmos.so"
   mkdir -p ~/.byond/bin
-  cp ./target/i686-unknown-linux-gnu/release/libauxmos.so ~/.byond/bin/libauxmos.so
+  cp libauxmos.so ~/.byond/bin/libauxmos.so
   echo "$AUXMOS_VERSION" > "$HOME/.byond/bin/auxmos-version.txt"
   ldd ~/.byond/bin/libauxmos.so
   echo "Finished installing Auxmos"
